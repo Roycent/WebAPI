46fb29529481dae68b731e1b19f95abacfd52dbb
Xing Zhihuan <xingzhihuan@buaa.edu.cn>
2018-06-06 14:51:08 +0800

    Users控制器增加api注释；增加注册功能；bug修复

--- WebApplication1/Controllers/UsersController.cs
+++ WebApplication1/Controllers/UsersController.cs
@@ -14,23 +14,38 @@ using WebAPI;
 
 namespace WebAPI.Controllers
 {
+    /// <summary>
+    /// 更改密码
+    /// </summary>
     public class PasswordModify
     {
         public string oldPasswd { get; set; }
         public string newPasswd { get; set; }
     }
+    /// <summary>
+    /// 用户信息相关控制器
+    /// </summary>
     public class UsersController : ApiController
     {
         private WebAPIEntities db = new WebAPIEntities();
 
         //controller前加Route,可以直接设置该函数的URL,比如下面的这个URL就是http://localhost:xxxx/Users/Login
         //调试web api可以用postman 
-        //body的类型选"x-www-form-urlencoded"
+        //body的类型选"raw" 并用json格式进行输入
         //下面是一个读取数据库并添加cookie的例子
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="user">UserName,Password
+        /// eg:{"UserName":"user1","Password":"123456"}
+        /// </param>
+        /// <returns>"failed":登录失败 "success":登录成功
+        /// </returns>
         [Route("User/Login")]
         public string Login(Users user)
         {
-            Users find = db.Users.First(Users => Users.UserName == user.UserName);
+            Users find = db.Users.FirstOrDefault(Users => Users.UserName == user.UserName);
             if (find == null)
             {
                 return "failed";
@@ -56,26 +71,59 @@ namespace WebAPI.Controllers
                 }
             }
         }
-
+        /// <summary>
+        /// 用户登出
+        /// </summary>
+        /// <param>无，直接读取cookie</param>
+        /// <returns>"success":登出成功；"failed":登出失败</returns>
         [Route("User/Logout")]
         public string Logout()
         {
-            HttpContext.Current.Response.Cookies.Remove("account");
+            try
+            {
+                HttpContext.Current.Response.Cookies.Remove("account");
+            }
+            catch
+            {
+                return "failed";
+            }
+
             return "success";
         }
-
+        /// <summary>
+        /// 当前用户信息
+        /// </summary>
+        /// <param>无，直接读取cookie</param>
+        /// <returns>用户所有信息,若无cookie则返回一个用户名为null的用户</returns>
         [Route("User/Info")]
-        public IHttpActionResult Info()
+        public Users Info()
         {
-            int userID = int.Parse(HttpContext.Current.Request.Cookies["account"]["UserID"]);
+            var cookie = HttpContext.Current.Request.Cookies["account"];
+            if (cookie == null)
+            {
+                Users nullUser = new Users();
+                nullUser.UserName = null;
+                return nullUser;
+            }
+            int userID = int.Parse(cookie["UserID"]);
             Users find = db.Users.Find(userID);
-            return Ok(find);
+            return find;
         }
 
+        /// <summary>
+        /// 更改密码
+        /// </summary>
+        /// <param name="pw">oldPasswd,newPasswd
+        /// eg:{"oldPasswd":"12345","newPasswd":"123456"}
+        /// </param>
+        /// <returns>"success":更改成功,"wrong password":更改失败,"error":错误（如登录态失效等）</returns>
         [Route("User/ModifyPassword")]
         public string ModifyPassword(PasswordModify pw)
         {
-            int userID =  int.Parse(HttpContext.Current.Request.Cookies["account"]["UserID"]);
+            var cookie = HttpContext.Current.Request.Cookies["account"];
+            if (cookie == null)
+                return "error";
+            int userID =  int.Parse(cookie["UserID"]);
             Users find = db.Users.Find(userID);
             if (find.Password == pw.oldPasswd)
             {
@@ -99,119 +147,42 @@ namespace WebAPI.Controllers
         {
             return HttpContext.Current.Request.Cookies["account"]["Email"].ToString();
         }
-
-        // GET: api/Users
-        public IQueryable<Users> GetUsers()
+        /// <summary>
+        /// 用户注册
+        /// </summary>
+        /// <param name="newUser">用户信息，必选字段为用户名、密码，可选字段为电话、邮箱等</param>
+        /// <returns>"success":注册成功  "user name exists":用户名存在  "error":错误</returns>
+        [Route("User/Register")]
+        public string Register(Users newUser)
         {
-            return db.Users;
-        }
-
-        // GET: api/Users/5
-        [ResponseType(typeof(Users))]
-        public IHttpActionResult GetUsers(long id)
-        {
-            Users users = db.Users.Find(id);
-            if (users == null)
+            string newUserName = newUser.UserName;
+            Users tryFind = db.Users.FirstOrDefault(Users => Users.UserName == newUserName);
+            if (tryFind != null)
             {
-                return NotFound();
+                return "user name exists";
             }
-
-            return Ok(users);
-        }
-
-        // PUT: api/Users/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutUsers(long id, Users users)
-        {
-            if (!ModelState.IsValid)
+            Random rd = new Random();
+            int r = rd.Next(1000);
+            long newUserID = (long)(DateTime.Now.ToUniversalTime() - new DateTime(2018, 3, 24)).TotalMilliseconds + r;
+            tryFind = db.Users.Find(newUserID);
+            while(tryFind != null)
             {
-                return BadRequest(ModelState);
+                r = rd.Next(1000);
+                newUserID = newUserID + r;
             }
-
-            if (id != users.UserID)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(users).State = EntityState.Modified;
-
+            newUser.UserID = newUserID;
+            newUser.integral = 100;//用户初始积分
             try
             {
+                db.Users.Add(newUser);
                 db.SaveChanges();
             }
-            catch (DbUpdateConcurrencyException)
+            catch
             {
-                if (!UsersExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return "error";
             }
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Users
-        [ResponseType(typeof(Users))]
-        public IHttpActionResult PostUsers(Users users)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Users.Add(users);
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateException)
-            {
-                if (UsersExists(users.UserID))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtRoute("DefaultApi", new { id = users.UserID }, users);
-        }
-
-        // DELETE: api/Users/5
-        [ResponseType(typeof(Users))]
-        public IHttpActionResult DeleteUsers(long id)
-        {
-            Users users = db.Users.Find(id);
-            if (users == null)
-            {
-                return NotFound();
-            }
-
-            db.Users.Remove(users);
-            db.SaveChanges();
-
-            return Ok(users);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool UsersExists(long id)
-        {
-            return db.Users.Count(e => e.UserID == id) > 0;
+            return "success";
         }
     }
 }
